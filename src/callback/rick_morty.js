bdc229da2fa2f0e7746f427769dc8d0e03db5309
alan <alanarostegui@hotmail.com>
2020-07-25 22:31:25 -0500

    resolver con promesas

--- src/callback/rick_morty.js
+++ src/callback/rick_morty.js
@@ -1,21 +1,6 @@
-let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 let API = "https://rickandmortyapi.com/api/character/";
 
-function fetchData(url, cb) {
-  let xhttp = new XMLHttpRequest();
-  xhttp.open("GET", url, true);
-  xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status === 200) {
-        cb(null, JSON.parse(xhttp.responseText));
-      } else {
-        const error = new Error("Error " + url);
-        return cb(error, null);
-      }
-    }
-  };
-  xhttp.send();
-}
+
 
 fetchData(API, function (error1, data1) {
   if (error1) return console.error(error1);
